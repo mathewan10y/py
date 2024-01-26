0e3143a07e212edf7969081456cd3f83afa0ac64
mathewan10y <mathewan10y@gmail.com>
2024-01-26 11:20:52 +0530

    n

--- check.py
+++ check.py
@@ -2,7 +2,7 @@ print('multiplication table')
 a=int(input('enter the number:'))
 print('\t  ******* MULTIPLICATION TABLE *******\n')
 for i in range(1,10):
-      s=a*i9
+      s=a*i
       print('\t\t',a,"  X  ",i,"  =  ",s)
 
       
